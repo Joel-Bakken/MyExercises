a50fd6ed24c4516699f1acbbd34cd3e78561cc1c
Joel Bakken <jkbetc@gmail.com>
2017-09-08 14:42:21 -0700

    create two new activities and begin filling out the logActivity

--- app/src/main/java/com/example/joelbakken/myexercises/MainActivity.java
+++ app/src/main/java/com/example/joelbakken/myexercises/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.joelbakken.myexercises;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mLogButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLogButton = (Button) findViewById(R.id.logButton);
+            mLogButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, LogActivity.class);
+                    startActivity(intent);
+                }
+            });
     }
 }
