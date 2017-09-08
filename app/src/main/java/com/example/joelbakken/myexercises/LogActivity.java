56109893c2b49bb08a38f0012d140541cbaa8780
Joel Bakken <jkbetc@gmail.com>
2017-09-08 15:35:59 -0700

    add temporary boiler plate exercises to the list

--- app/src/main/java/com/example/joelbakken/myexercises/LogActivity.java
+++ app/src/main/java/com/example/joelbakken/myexercises/LogActivity.java
@@ -2,12 +2,17 @@ package com.example.joelbakken.myexercises;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LogActivity extends AppCompatActivity {
+    private Button mSubmitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
+
+        mSubmitButton = (Button) findViewById(R.id.submitButton);
+            mSubmitButton.setOnClickListener();
     }
 }
--- app/src/main/java/com/example/joelbakken/myexercises/ViewActivity.java
+++ app/src/main/java/com/example/joelbakken/myexercises/ViewActivity.java
@@ -2,8 +2,20 @@ package com.example.joelbakken.myexercises;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class ViewActivity extends AppCompatActivity {
+    GridView gridView;
+    String[] exercise = new String[] {
+            "Jogging", "Running", "Swimming"};
+    String[] date = new String[] {
+            "6/8/2017", "7/1/2017", "9/1/2017"};
+    String[] startTime = new String[] {
+            "13:00", "11:15", "12:00"};
+    String[] endTime = new String[]{
+            "14:10", "12:00", "13:00"};
+    String[] notes = new String[] {
+            "Blah Blah Blah", "I went far and fast", "The water was pretty wet"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
