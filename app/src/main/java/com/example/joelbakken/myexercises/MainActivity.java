bb27856cc1e0aaaa4aab18fd8ddb986418a63519
Joel Bakken <joel@>
2017-09-11 15:14:08 -0700

    add butterknife and refactor mainactivity and viewactivity

--- app/src/main/java/com/example/joelbakken/myexercises/MainActivity.java
+++ app/src/main/java/com/example/joelbakken/myexercises/MainActivity.java
@@ -1,38 +1,42 @@
 package com.example.joelbakken.myexercises;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 
 public class MainActivity extends AppCompatActivity {
-    private Button mLogButton;
-    private Button mViewButton;
+    @Bind(R.id.logButton) Button mLogButton;
+    @Bind(R.id.viewButton) Button mViewButton;
+    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        mLogButton = (Button) findViewById(R.id.logButton);
-            mLogButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(MainActivity.this, LogActivity.class);
-                    startActivity(intent);
-                }
-            });
+       mLogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LogActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        mViewButton = (Button) findViewById(R.id.viewButton);
-            mViewButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(MainActivity.this, ViewActivity.class);
-                    startActivity(intent);
-                }
-            });
+        mViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ViewActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/joelbakken/myexercises/ViewActivity.java
+++ app/src/main/java/com/example/joelbakken/myexercises/ViewActivity.java
@@ -1,11 +1,20 @@
 package com.example.joelbakken.myexercises;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class ViewActivity extends AppCompatActivity {
-    private ListView mlistView;
+    @Bind(R.id.exerciseLog) TextView mExerciseLog;
+    @Bind(R.id.listView) ListView mListView;
     String[] exercise = new String[] {
             "Jogging", "Running", "Swimming"};
     String[] date = new String[] {
@@ -21,8 +30,22 @@ public class ViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
+        ButterKnife.bind(this);
 
-        mlistView = (ListView) findViewById(R.id.exercises);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, exercise);
+        mListView.setAdapter(adapter);
 
+        mListView.setOnClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String exercise = ((TextView)view).getText().toString();
+                Toast.makeText(ViewActivity.this, exercise,
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Intent intent = getIntent();
+        String exercise = intent.getStringExtra("exercise");
+        mExerciseLog.setText("On this date you did" + exercise + "for exercise.");
     }
 }
