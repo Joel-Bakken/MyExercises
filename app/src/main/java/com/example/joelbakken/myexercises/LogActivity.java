5b502f309d5e98aa2c2e7b695efc16398b8d6029
Joel Bakken <joel@>
2017-09-11 15:52:19 -0700

    add a button that does nothing for right now as we have not covered this in our lessons yet to the logactivity page

--- app/src/main/java/com/example/joelbakken/myexercises/LogActivity.java
+++ app/src/main/java/com/example/joelbakken/myexercises/LogActivity.java
@@ -2,16 +2,26 @@ package com.example.joelbakken.myexercises;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class LogActivity extends AppCompatActivity {
-    private Button mSubmitButton;
+
+    @Bind(R.id.submitButton) Button mSubmitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
 
-
+        mSubmitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
