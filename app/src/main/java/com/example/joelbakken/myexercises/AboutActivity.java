f811f6fa6de5e3be7a7a339a96e982698030159b
Joel Bakken <jkbetc@gmail.com>
2017-09-13 09:43:32 -0700

    add buttons to the about screen and a nice picture too

--- app/src/main/java/com/example/joelbakken/myexercises/AboutActivity.java
+++ app/src/main/java/com/example/joelbakken/myexercises/AboutActivity.java
@@ -1,13 +1,41 @@
 package com.example.joelbakken.myexercises;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class AboutActivity extends AppCompatActivity {
+    @Bind(R.id.logButton) Button mLogButton;
+    @Bind(R.id.viewButton) Button mViewButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        ButterKnife.bind(this);
+
+
+        mLogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AboutActivity.this, LogActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AboutActivity.this, ViewActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
