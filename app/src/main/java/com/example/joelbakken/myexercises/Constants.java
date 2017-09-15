067f9ab859942b5f61ca5c31bdfcea3e2ff7305b
Joel Bakken <jkbetc@gmail.com>
2017-09-15 13:38:03 -0700

    add yelp api data

---
+++ app/src/main/java/com/example/joelbakken/myexercises/Constants.java
@@ -0,0 +1,10 @@
+package com.example.joelbakken.myexercises;
+
+
+
+public class Constants {
+    public static final String YELP_CONSUMER_KEY = BuildConfig.YELP_CONSUMER_KEY;
+    public static final String YELP_CONSUMER_SECRET = BuildConfig.YELP_CONSUMER_SECRET;
+    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
+    public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
+}
